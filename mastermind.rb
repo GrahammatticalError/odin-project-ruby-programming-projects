28dd65125bdf5bc98da8242558c45deb7b6fc36d
Matthew Graham <graham.matt5@gmail.com>
2021-11-19 09:41:01 -0500

    Added a HumanPlayer class, a Game class, and edited the code for the ComputerPlayer class.

--- mastermind.rb
+++ mastermind.rb
@@ -1,8 +1,46 @@
-class ComputerCode
-    attr_reader :code
+class ComputerPlayer
+    attr_reader :code, :guess
     
     def initialize
-        @code = [rand(6)+1, rand(6)+1, rand(6)+1, rand(6)+1]
+    end
+
+    def computer_code
+        return [rand(6)+1, rand(6)+1, rand(6)+1, rand(6)+1]
+    end
+
+    def computer_guess
+        return "#{rand(6)+1}#{rand(6)+1}#{rand(6)+1}#{rand(6)+1}"
+    end
+
+end
+
+class HumanPlayer
+    attr_reader :player_guess
+
+    def initialize
+        @player_guess = nil
+    end
+
+    def take_guess
+        @player_guess = gets.chomp
+        @player_guess = @player_guess.split("")
+    end
+
+end
+
+class Game
+    attr_reader :code
+    def initialize
+        @code = nil
+        @turns = 12
+    end
+
+    def take_code(code)
+        @code = code
+    end
+
+    def check_guess(guess)
+        
     end
 
 end
